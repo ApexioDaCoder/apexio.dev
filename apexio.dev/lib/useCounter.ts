3e5d35b327a26b61e36ba8afacb1a1792e405776
ApexioDaCoder <apexiodacoder@gmail.com>
2021-12-29 04:51:00 +0000

    added fallback for vercel

--- apexio.dev/lib/useCounter.ts
+++ apexio.dev/lib/useCounter.ts
@@ -1,29 +1,44 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from "react";
 
-export default function useCounter(): [number, boolean, () => void, () => void] {
-  const [count, setCount] = useState(0)
-  const [isSyncing, setIsSyncing] = useState(true)
+export default function useCounter(): [
+  number,
+  boolean,
+  () => void,
+  () => void
+] {
+  const [count, setCount] = useState(0);
+  const [isSyncing, setIsSyncing] = useState(true);
   const increase = useCallback(() => {
-    setCount(n => n + 1)
-    fetch('/api/counter/increase').catch(e => console.error(e))
-  }, [])
+    setCount((n) => n + 1);
+    fetch("/api/counter/increase").catch((e) => {
+      console.error(e);
+      localStorage.setItem("count", count);
+    });
+  }, []);
   const decrease = useCallback(() => {
-    setCount(n => n - 1)
-    fetch('/api/counter/decrease').catch(e => console.error(e))
-  }, [])
+    setCount((n) => n - 1);
+    fetch("/api/counter/decrease").catch((e) => {
+      console.error(e);
+      localStorage.setItem("count", count);
+    });
+  }, []);
 
   useEffect(() => {
-    fetch('/api/counter').then(resp => resp.json().catch(() => ({})))
+    fetch("/api/counter")
+      .then((resp) => resp.json().catch(() => ({})))
       .then(({ count }) => {
-        if (typeof count === 'number' && !Number.isNaN(count)) {
-          setCount(count)
+        if (typeof count === "number" && !Number.isNaN(count)) {
+          setCount(count);
         }
       })
-      .catch(e => console.error(e))
-      .finally(() => {
-        setIsSyncing(false)
+      .catch((e) => {
+        console.error(e);
+        setCount(parseInt(localStorage.getItem("count") || "0"));
       })
-  }, [])
+      .finally(() => {
+        setIsSyncing(false);
+      });
+  }, []);
 
-  return [count, isSyncing, increase, decrease]
+  return [count, isSyncing, increase, decrease];
 }
